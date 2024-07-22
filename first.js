37263641b5dfc16287234988fe5cc9ad124bd4ea
saikat7890 <saikathazra9732@gmail.com>
2024-07-22 17:01:25 +0530

    HTTP Server Created

--- first.js
+++ first.js
@@ -1,2 +1,21 @@
 console.log("Hello first.js");
 console.log("oooo");
+// console.log(global);
+// console.log(__dirname);
+// console.log(__filename);
+
+const { log } = require("console");
+// const {add, multiply} = require("./math");
+// console.log(add(2, 3));
+
+const os = require("os");
+const path = require("path");
+
+// console.log(os.arch());
+// console.log(os.type());
+// console.log(os.cpus());
+// console.log(os.machine());
+// console.log(os.totalmem());
+
+console.log(path.join(__dirname, "apps", "myapp"));
+console.log(path.parse(__filename));
